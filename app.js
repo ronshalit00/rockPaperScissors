a1f8f7e56eb83a40a3c866e83dd0557231b21d22
ronshalit00 <ronshalit00@gmail.com>
2023-04-19 13:04:35 +0300

    Changes the project to have a GUI

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
-let winsCount = 0;
-let loseCount = 0;
-let roundCount = 1;
+// Element selectors
+const buttons = document.querySelectorAll(".button");
+const roundResult = document.querySelector("#round-result");
+const gameResults = document.querySelector("#game-results");
+const winsText = document.querySelector("#wins-count");
+const tiesText = document.querySelector("#ties-count");
+const lossText = document.querySelector("#loss-count");
 
+// The click event
+const clickEvent = (e) => {
+	roundResult.textContent = `${playRound(
+		`${e.target.id}`,
+		getComputerChoice()
+	)}`;
+	if (result === "win") {
+		winsText.textContent = winsCount;
+	} else if (result === "tie") {
+		tiesText.textContent = tiesCount;
+	} else {
+		lossText.textContent = loseCount;
+	}
+	roundResult.classList.remove("hidden");
+
+	if (winsCount == 5) {
+		alert("You won the game!");
+		window.location.reload();
+	} else if (loseCount == 5) {
+		alert("You lose!");
+		window.location.reload();
+	}
+};
+
+// Event listener for clicking the buttons, initiate the click event
+buttons.forEach((button) => {
+	button.addEventListener("click", clickEvent);
+});
+
+// Get a random computer choice
 function getComputerChoice() {
 	const choicesArr = ["rock", "paper", "scissors"];
 	const choice = Math.floor(Math.random() * choicesArr.length);
@@ -9,6 +43,13 @@ function getComputerChoice() {
 	return choicesArr[choice];
 }
 
+// Initiate counts
+let winsCount = 0;
+let tiesCount = 0;
+let loseCount = 0;
+let result = "";
+
+// Play a round
 function playRound(playerSelection, computerSelection) {
 	if (playerSelection == null) {
 		return;
@@ -18,68 +59,45 @@ function playRound(playerSelection, computerSelection) {
 
 	if (playerChoice === "rock") {
 		if (computerChoice === "rock") {
-			roundCount++;
+			result = "tie";
+			tiesCount++;
 			return "It's a tie! Rock vs Rock";
 		} else if (computerChoice === "paper") {
-			roundCount++;
+			result = "lose";
 			loseCount++;
 			return "You lose! Paper beats Rock";
 		} else {
-			roundCount++;
+			result = "win";
 			winsCount++;
 			return "You won! Rock beats Scissors";
 		}
 	} else if (playerChoice === "paper") {
 		if (computerChoice === "rock") {
-			roundCount++;
+			result = "win";
 			winsCount++;
 			return "You won! Paper beats Rock";
 		} else if (computerChoice === "paper") {
-			roundCount++;
+			result = "tie";
+			tiesCount++;
 			return "It's a tie! Paper vs Paper";
 		} else {
-			roundCount++;
+			result = "lose";
 			loseCount++;
 			return "You lose! Scissors beats Paper";
 		}
 	} else if (playerChoice === "scissors") {
 		if (computerChoice === "rock") {
-			roundCount++;
+			result = "lose";
 			loseCount++;
 			return "You lose! Rock beats Scissors";
 		} else if (computerChoice === "paper") {
-			roundCount++;
+			result = "win";
 			winsCount++;
 			return "You won! Scissors beats Paper";
 		} else {
-			roundCount++;
+			result = "tie";
+			tiesCount++;
 			return "It's a tie! Scissors vs Scissors";
 		}
-	} else {
-		return "Failed attempt, please choose between 'Rock', 'Paper' or 'Scissors'";
 	}
-}
-
-function game() {
-	while (roundCount < 6 && winsCount < 3) {
-		const playerChoice = prompt(
-			"Please choose between rock, paper or scissors:"
-		);
-		console.log(`Round ${roundCount}:`);
-		console.log(playRound(playerChoice, getComputerChoice()));
-	}
-	if (winsCount > loseCount || winsCount > 2) {
-		console.log(
-			`WINNER WINNER CHICKEN DINNER! You won the game with ${winsCount} rounds won!`
-		);
-	} else if (winsCount === loseCount) {
-		console.log(`You're tied with ${winsCount} wins!`);
-	} else {
-		console.log(
-			`SUCH A SHAME! You lose the game with ${winsCount} rounds won.`
-		);
-	}
-	roundCount = 1;
-	winsCount = 0;
-	loseCount = 0;
 }
